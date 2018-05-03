db6251c85e652102ecf37badcb0798c3fb3e2793
1saulsh <saulsh@yahoo.com>
2018-05-02 22:23:07 -0600

    100% functionality of customer purchasing product and inventory list updating

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,23 +12,26 @@ var connection = mysql.createConnection({
     database: "amazon_db"
 });
 
-//logs wether you are connected or not and what is your connection id
-connection.connect();
-    
+//logs wether you are connected or not and what is your connection id.    //testing connectivity to mysql database
+connection.connect(function(error) {
+    if (error) throw err; 
     //displays our initial function
     displayTable();
     
-//quering data from mySQL
+});
+    
+    
+//quering data from mySQL to show the table with the products for the customer to view
 function displayTable() {
     connection.query("Select * FROM products", function(error, results) { 
         if (error) throw error;
-    //testing connectivity to mysql database
-    //console.log("Connection successful!");
+   
     
     var t = new Table
 
     //creating columns and quering data and inputting data into table (info retrieved from easy-table npm documentation)
     // this data will be displayed in a table on the command line
+    //forEach is the way to loop through info in mySQL
     results.forEach(function(product) {
         t.cell("Item", product.id);
         t.cell("Product", product.productName);
@@ -38,44 +41,73 @@ function displayTable() {
         t.newRow();
     });
     console.log(t.toString());
-})     
-    inquirer.prompt([{
+
+    pickProduct();
+    })
+}
+
+//pick a procduct and how many
+function pickProduct(answers) {
+     inquirer.prompt([
+     {
         name:"id",
         type: "input",
         message: "What is the ID of the item you would like to buy?"
-    }, {
-
-        name:"quanity",
+    }, 
+      {
+        name:"quantity",
         type: "input",
         message: "Please input the quantity of the item you would like to buy."
-    }]).then(function(answers){
 
-        //connect input to database functinality not working. I need a refresher on CRUD
+      }
+     ]).then(function(answers){
+
+        //connect input to database functionality not working.
         connection.query("SELECT * FROM products WHERE id = " + answers.id, function(error, results) {
-            try {
-                let price = results[0].price;
-                var total = (answers.stockQuantity * price).toFixed(2)
+            
+            console.log("Quantity " + answers.quantity);
 
-                if(results[0].stockQuantity < answers.stockQuantity) {
-                    console.log("Sorry, not enough inventory on hand");
-                    displayTable();
-                } else {
-                    connection.query("Update products stockQuantity - stockQuantity" + answers.stockQuantity + "where id" + answers.id, function(error, results) {
-                        console.log("Inventory updated!");
-                        console.log("Your total is: $ " + total);
-                        console.log("Thank you for your business! Enjoy your new products!");
-                    });
-                }
-            }catch(e){
-                console.log("there was an error with your request: ", e.message);
-                exitProgram();
+            if (parseInt(answers.quantity) > results[0].stockQuantity){
+           
+            console.log("Sorry, there are only " + results[0].stockQuantity + " left");
+            console.log("Please select a smaller quantity")
+                pickProduct();
+ 
             }
-        })
-    })
-}
+            else {
+                console.log("Your purchase of " + answers.quantity + ' ' + results[0].productName +"/s total cost is: $ " + parseInt(results[0].price) * parseInt(answers.quantity));
+                console.log("Thank you for your business! Enjoy your new products!");
+
+            var quantityLeft = results[0].stockQuantity - answers.quantity;
+                console.log(quantityLeft);
+                connection.query(
+                    "UPDATE products SET ? WHERE?",
+                    [
+                        {stockQuantity: quantityLeft
+                        },
+                        {
+                            id: answers.id
+                        }
+                    ],
+                    function(error) {
+                        if (error) throw error;
+                    });
+                    console.log("Inventory updated. There are " + quantityLeft + " left");
+                    displayTable();
+                    exitProgram();
+                }
+
+            })
+        });
+
+};
+        
+
+
+
 function exitProgram(){
     connection.end();
 }
-
+    
 
 
